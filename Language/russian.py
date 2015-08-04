2a9ab0e503397f7c1bced6fb42a610c9e7fed66d
Fabian Dill <fabian.dill@web.de>
2015-08-04 22:54:09 +0200

    Russian update by Dr. Lapis

--- Language/russian.py
+++ Language/russian.py
@@ -1,18 +1,20 @@
+__author__ = "Dr. Lapis"
 langversion = 1
-langname = "Russian"
+langname = "Русский"
 
 ##updater
 # text construct: "Version "+version+available+changelog
 #example: Version 3 available, click here to download, or for changelog click here
-available = " доступна, кликните, чтобы скачать"
-changelog = ", или посмотрите список изменений тут"
+available = " доступна, нажмите здесь, чтобы скачать"
+changelog = ", или нажмите здесь, чтобы посмотреть список изменений"
 
 ##world gen
-worldify = "world from image"
-planetoids = "Planetoids & Terra"
-arena = "Dungeon Arena"
-flat = "Flatworld"
+worldify = "из картинки"
+planetoids = "Планетоиды и Терра"
+arena = "Данжевая Арена"
+flat = "Плоский мир"
 new = "Новый мир:"
+
 
 ##mainmenu
 
@@ -22,126 +24,131 @@ exit = "Выход"
 
 #start
 start = "Старт"
-terraria = "Terraria"
-steamfree = "Terraria Steamfree"
+terraria = "Террария"
+steamfree = "Террария без Стима"
 
 #open
 open = "Открыть"
-imagefolder = "Изображения миров"
-backupfolder = "Бекапы миров"
+imagefolder = "Картинки Миров"
+backupfolder = "Откаты Миров"
 themes = "Темы Omnitool"
 
 #visit
-visit = "Дополнительно"
-donate = "Помощь"
-homepage = "Omnitool"
-TO = "Terraria Online"
-wiki = "Terraria Wiki"
-##GO = "Terrariago" terrariago.ru
+visit = "Посетить"
+donate = "Пожертвовать"
+homepage = "Домашняя страница Omnitool"
+TO = "Форум Terraria Online"
+wiki = "Англоязычная Террария Вики"
+
 
 ##world thumbnail
 label = "Мир: "
 
+
 ##settings menu
-none = "None"
-warning = "Для сохранения изменений перезагрузите программу" #out of date - not all changes require a restart now
-world_columns = "World Columns:"
-tiny = "Крошечный"  #unused
+warning = "Для того, чтобы некоторые изменения вступили в силу, надо перезагрузить Omnitool"
+
+none = "Нет"
+tiny = "Микро" #unused
 small = "Маленький"
-medium = "Средний"
+medium = "Нормальный"
 large = "Большой"
 very_large = "XXL"
 
-theme_select = "Выберите тему:"
-thumbsize = "Размер миниатюры:"
-mk_backups = "Создать резервную копию"
+theme_select = "Выбор темы:"
+thumbsize = "Размер миниатюры мира:"
+mk_backups = "Сделать Откаты:"
+world_columns = "Количество Колонок Миров:"
 
 ##world interaction menu
-wa_worldactionmenu = "Action for {}:"
-wa_imageopen = "Open Image"
-wa_renderopen = "Render World"
-wa_teditopen = "Open in TEdit"
-wa_update = "Update Image"
-wa_super = "Generate Super-Image"
+wa_worldactionmenu = "Действие для {}:"
+wa_imageopen = "Открыть картинку"
+wa_renderopen = "Отрендерить Мир"
+wa_teditopen = "Открыть в TEdit"
+wa_update = "Обновиь Картинку"
+wa_super = "Сгенерировать Супер Картинку"
 
 ##planetoids & terra
-pt_start = 'Создать!'
+pt_start = 'Начать генерацию!'
 pt_name = "Имя: "
 pt_mode = "Режим: "
-pt_mirror = "Mirror Mode: "
-pt_small = "маленький Planetoids"
-pt_medium = "средний Planetoids"
-pt_large = "большой Planetoids"
-pt_square = "квадратный Planetoids"
-pt_both = "большие Planetoids & Terra"
-pt_square_terra = "квадратная Terra"
 
-pt_start_sel = "Старт: "
+pt_small = "Планетоиды (маленький мир)"
+pt_medium = "Планетоиды (средний мир)"
+pt_large = "Планетоиды (большой мир)"
+pt_square = "Планетоиды (квадратный мир)"
+pt_both = "Терра & Планетоиды (большой мир)"
+pt_square_terra = "Терра (квадратный мир)"
+
+pt_start_sel = "Начальное время суток: "
 pt_morning = "Утро"
 pt_day = "День"
 pt_night = "Ночь"
-pt_bloodmoon = "Кровавая луна"
+pt_bloodmoon = "Кровавая Луна"
 
 pt_extras = "Дополнительно: "
 pt_sun = "Солнце: "
-pt_atlantis = "Атлантида: "
-pt_merchant = "Торговец: "
-pt_lloot = "Мало лута: "
+pt_atlantis = "Водный Мир: "
+pt_merchant = "Торговец сначала: "
+pt_lloot = "Меньше вещей в сундуках: "
+pt_mirror = "Зеркальный режим: "
+pt_pre = "Префиксы Вещей: "
 
 ##worldify
-w_start = "Начать!"
+w_start = "Начать превращение картинки в мир!"
 w_cont = "Продолжить"
 w_name = "Имя: "
 w_rgb = "RGB"
-w_hsv = "weighted HSV"
+w_hsv = "утяжелённый HSV"
 w_method = "Метод: "
 
-w_priority = "Priority selection"
+w_priority = "Выбрать Приоритет:"
 w_hue = "Оттенок: "
 w_saturation = "Насыщенность: "
 w_brightness = "Яркость: "
 
+
 ##arena
-a_start = "Начать!"
+a_start = "Начать генерацию!"
 a_name = "Имя: "
-a_rooms = "Комнаты: "
-a_sidelen = "Длина сторон комнат: "
-a_corlen = "Длина коридора: "
-a_chest = "Сундуки: "
-a_itemchest = "Вещей в сундуках: "
+a_rooms = "Кол-во комнат: "
+a_sidelen = "Длина комнат: "
+a_corlen = "Длина коридоров: "
+a_chest = "Вероятность появления сундука в комнате: "
+a_itemchest = "Вещей в каждом сундуке: "
 a_light = "Освещение: "
-a_chances = "Количество комнат: "
-a_standard = "Стандарт: "
-a_cross = "Пересечения коридоров: "
+a_chances = "Шансы генерации комнаты как: "
+a_standard = "Стандартной: "
+a_cross = "Перекрёсток: "
 
 ##torch
-at_chances = "Color chances:"
-at_full = "Full spectrum"
-at_blue = "Blue"
-at_red = "Red"
-at_green = "Green"
-at_pink = "Demon"
-at_white = "White"
-at_yellow = "Yellow"
-at_purple = "Purple"
-at_lime = "Cursed"
+at_chances = "Шанс генерации освещения различных цветов:"
+at_full = "Полный спектр"
+at_blue = "Синий"
+at_red = "Красный"
+at_green = "Зелёный"
+at_pink = "Демоничемкий"
+at_white = "Белый"
+at_yellow = "Жёлтый"
+at_purple = "Фиолетовый"
+at_lime = "Проклятый"
 
 ##plugins
-pl_start = "Start plugin"
-pl_rec = "Select a world to be received"
-pl_mod = "Select a world to be modified"
-pl_trans = "Select two worlds to be used for a transfer"
-pl_trans_source = "Source"
-pl_trans_target = "Target"
+pl_start = "Запустить плагин"
+pl_rec = "Выберите мир для получения"
+pl_mod = "Выберите мир для изменения"
+pl_trans = "Выберите два мира для трансфера"
+pl_trans_source = "Источник"
+pl_trans_target = "Цель"
 
 
 ##flatworld
-fw_size = "World size:"
-fw_tiny = "tiny"
-fw_square = "square"
-fw_small = "small"
-fw_medium = "medium"
-fw_large = "large"
-fw_tile = "Tile type:"
-fw_wall = "Wall type:"
-fw_surf = "Surface type:"
+fw_size = "Размер мира:"
+fw_tiny = "микро"
+fw_square = "квадратный"
+fw_small = "маленький"
+fw_medium = "средний"
+fw_large = "большой"
+fw_tile = "Тип блоков:"
+fw_wall = "Тип стен:"
+fw_surf = "Тип поверхности:"
